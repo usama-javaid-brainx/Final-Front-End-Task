dcbccd70ebb97be48f33cead0af603e978aa38cd
Usama Javaid <usama.javaid@brainxtech.com>
2020-12-29 22:01:13 +0500

    days list added

--- js/freshleyscript.js
+++ js/freshleyscript.js
@@ -1,3 +1,80 @@
+$(document).ready(function () {
+  $("#meal-6").css({ "background-color": "#07872b", color: "white" });
+  $("#menu-4-list").hide();
+  $("#menu-10-list").hide();
+  $("#menu-12-list").hide();
+  //listeners on meal
+  $("#meal-4").click(function () {
+    MealListener(
+      "#meal-4",
+      "#meal-6",
+      "#meal-10",
+      "#meal-12",
+      "#menu-4-list",
+      "#menu-6-list",
+      "#menu-10-list",
+      "#menu-12-list"
+    );
+  });
+
+  $("#meal-6").click(function () {
+    MealListener(
+      "#meal-6",
+      "#meal-4",
+      "#meal-10",
+      "#meal-12",
+      "#menu-6-list",
+      "#menu-4-list",
+      "#menu-10-list",
+      "#menu-12-list"
+    );
+  });
+  $("#meal-10").click(function () {
+    MealListener(
+      "#meal-10",
+      "#meal-4",
+      "#meal-6",
+      "#meal-12",
+      "#menu-10-list",
+      "#menu-4-list",
+      "#menu-6-list",
+      "#menu-12-list"
+    );
+  });
+  $("#meal-12").click(function () {
+    MealListener(
+      "#meal-12",
+      "#meal-4",
+      "#meal-10",
+      "#meal-6",
+      "#menu-12-list",
+      "#menu-4-list",
+      "#menu-10-list",
+      "#menu-6-list"
+    );
+  });
+  function MealListener(
+    show1,
+    hide1,
+    hide2,
+    hide3,
+    showinfo,
+    hideinfo1,
+    hideinfo2,
+    hideinfo3
+  ) {
+    $(show1).css({ "background-color": "#07872b", color: "white" });
+    $(hide1).css({ "background-color": "white", color: "#333" });
+    $(hide2).css({ "background-color": "white", color: "#333" });
+    $(hide3).css({ "background-color": "white", color: "#333" });
+    //hide show
+    $(showinfo).show();
+    $(hideinfo1).hide();
+    $(hideinfo2).hide();
+    $(hideinfo3).hide();
+  }
+});
+
 var form = $("#my-form");
 form.validate({
   errorPlacement: function errorPlacement(error, element) {
